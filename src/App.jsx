7a265e30ca24719c79427b53fa03ab46b2b30dfc
Akhil Babu <akhilbabu123@gmail.com>
2025-12-18 15:29:59 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LightSystem from './LightSystem'
+
+function App() {
+  return (
+    <div>
+      <LightSystem/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/LightSystem.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+function LightSystem() {
+    const [active, setActive] = useState([])
+
+    const handleClick = (i) => {
+    if (active.includes(i)) return
+
+    const updated = [...active, i]
+    setActive(updated)
+
+    if (updated.length === 9) {
+      let stack = [...updated]
+
+      const interval = setInterval(() => {
+        stack.pop()
+        setActive([...stack])
+
+        if (stack.length === 0) {
+          clearInterval(interval)
+        }
+      }, 500)
+    }
+  }
+
+  return (
+    <div
+    style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 80px)",
+        gap: "10px",
+    }}
+    >
+      {Array.from({length: 9}).map((_,index)=>(
+        <div
+        key={index}
+        onClick={()=> handleClick(index)}
+         style={{
+            width: "80px",
+            height: "80px",
+            border: "2px solid black",
+            backgroundColor: active.includes(index)
+              ? "red"
+              : "white",
+          }}
+        >
+
+        </div>
+      ))} 
+    </div>
+  )
+}
+
+export default LightSystem
+
